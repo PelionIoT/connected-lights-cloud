888204cb64bf692ada46330e94f8c08a97ddd042
Jenny Plunkett <jennifer.plunkett@arm.com>
2020-08-17 14:50:33 -0500

    Bring cloud files up to date

--- update_default_resources.c
+++ update_default_resources.c
@@ -1,16 +1,20 @@
-//----------------------------------------------------------------------------
-// The confidential and proprietary information contained in this file may
-// only be used by a person authorised under and to the extent permitted
-// by a subsisting licensing agreement from ARM Limited or its affiliates.
+// ----------------------------------------------------------------------------
+// Copyright 2016-2017 ARM Ltd.
 //
-// (C) COPYRIGHT 2016 ARM Limited or its affiliates.
-// ALL RIGHTS RESERVED
+// SPDX-License-Identifier: Apache-2.0
 //
-// This entire notice must be reproduced on all copies of this file
-// and copies of this file may only be made by a person if such person is
-// permitted to do so under the terms of a subsisting license agreement
-// from ARM Limited or its affiliates.
-//----------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------
 
 #ifdef MBED_CLOUD_CLIENT_USER_CONFIG_FILE
 #include MBED_CLOUD_CLIENT_USER_CONFIG_FILE
@@ -18,18 +22,20 @@
 
 #include <stdint.h>
 
-const uint8_t arm_uc_vendor_id[16] = { 0 };
+#ifdef MBED_CLOUD_DEV_UPDATE_ID
+const uint8_t arm_uc_vendor_id[16] = { "dev_manufacturer" };
 const uint16_t arm_uc_vendor_id_size = sizeof(arm_uc_vendor_id);
 
-const uint8_t arm_uc_class_id[16]  = { 0 };
+const uint8_t arm_uc_class_id[16]  = { "dev_model_number" };
 const uint16_t arm_uc_class_id_size = sizeof(arm_uc_class_id);
+#endif
 
-const uint8_t arm_uc_device_id[16] = { 0 };
-const uint16_t arm_uc_device_id_size = sizeof(arm_uc_device_id);
-
-const uint8_t arm_uc_default_fingerprint[32] =  { 0 };
+#ifdef MBED_CLOUD_DEV_UPDATE_CERT
+const uint8_t arm_uc_default_fingerprint[32] = { 0 };
 const uint16_t arm_uc_default_fingerprint_size =
     sizeof(arm_uc_default_fingerprint);
 
 const uint8_t arm_uc_default_certificate[1] = { 0 };
-const uint16_t arm_uc_default_certificate_size = sizeof(arm_uc_default_certificate);
+const uint16_t arm_uc_default_certificate_size =
+    sizeof(arm_uc_default_certificate);
+#endif
